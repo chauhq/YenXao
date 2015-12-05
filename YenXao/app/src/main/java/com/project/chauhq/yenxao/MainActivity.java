fcb6a91e2365eeec0ad74172fe0fca6eaf0fffaa
Hoang Quang Chau <chauhq@asiantech.vn>
2015-12-05 09:19:58 +0700

    first comment

---
+++ YenXao/app/src/main/java/com/project/chauhq/yenxao/MainActivity.java
@@ -0,0 +1,9 @@
+package com.project.chauhq.yenxao;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+}
